8f458d30819e741cd48c9d9a993a22ce4ff46048
Stephen Kennedy <stephen.kennedy@siriusxm.com>
2026-06-11 20:05:39 +0100

    fix python

--- components/minirig_ble/__init__.py
+++ components/minirig_ble/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor, switch, esp32_ble_tracker
+from esphome.components import sensor, esp32_ble_tracker
 from esphome.const import (
     CONF_ID,
     CONF_NAME,
@@ -12,7 +12,7 @@ from esphome.const import (
 )
 
 DEPENDENCIES = ["esp32_ble_tracker"]
-AUTO_LOAD = ["sensor", "switch"]
+AUTO_LOAD = ["sensor"]
 MULTI_CONF = True
 
 minirig_ble_ns = cg.esphome_ns.namespace("minirig_ble")
@@ -22,7 +22,6 @@ MinirigBLEComponent = minirig_ble_ns.class_(
 
 CONF_MINIRIGS = "minirigs"
 CONF_BATTERY_SENSOR = "battery_sensor"
-CONF_CHARGE_SWITCH = "charge_switch"
 CONF_V_MIN = "v_min"
 CONF_V_MAX = "v_max"
 
@@ -37,9 +36,6 @@ MINIRIG_SCHEMA = cv.Schema(
             entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
             accuracy_decimals=0,
         ),
-        cv.Optional(CONF_CHARGE_SWITCH): switch.switch_schema(
-            MinirigBLEComponent  # placeholder, overridden per instance
-        ),
         cv.Optional(CONF_V_MIN, default=10560): cv.int_range(min=8000, max=12000),
         cv.Optional(CONF_V_MAX, default=12582): cv.int_range(min=10000, max=15000),
     }
@@ -50,7 +46,7 @@ CONFIG_SCHEMA = cv.Schema(
         cv.GenerateID(): cv.declare_id(MinirigBLEComponent),
         cv.Required(CONF_MINIRIGS): cv.ensure_list(MINIRIG_SCHEMA),
     }
-).extend(cv.polling_component_schema("5min"))
+).extend(cv.polling_component_schema("5min")).extend(esp32_ble_tracker.ESP_BLE_DEVICE_SCHEMA)
 
 
 async def to_code(config):
